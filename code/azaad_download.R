5910833bf40cddd198de94202fa409503b0aefd9
Anshul Gupta <anshul.dlf@gmail.com>
2014-04-01 11:57:13 -0400

    Downloading AAP for 30 minutes

--- code/azaad_download.R
+++ code/azaad_download.R
@@ -5,10 +5,10 @@ library(streamR)
 load("my_oauth")
 
 # Set the two terms
-term1 <- "modi"
-term2 <- "bjp"
+term1 <- "aamaadmiparty"
+term2 <- "kejriwal"
 
 # capturing 2 minutes of tweets sent from India
-filterStream(file.name="data/tweets_2.json",track=c(term1, term2),  locations=c(68.17, 6.75, 97.4, 35.99), timeout=600, oauth=my_oauth)
+filterStream(file.name="data/tweets_d1.json",track=c(term1, term2),  locations=c(68.17, 6.75, 97.4, 35.99), timeout=1800, oauth=my_oauth)
 # parsing tweets into dataframe
-tweets.df <- parseTweets("data/tweets_2.json", verbose = TRUE)
+tweets.df <- parseTweets("data/tweets_d1.json", verbose = TRUE)
